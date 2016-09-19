c8a8d96cb71429f14db2a16792a6dc57f371ac3e
David Spreekmeester <david@grrr.nl>
2016-09-19 17:53:46 +0200

    Added basic call to BLC library

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 #! /usr/bin/env node
-console.log('Hello world');
+var blc = require("broken-link-checker");
+
+var siteUrl = 'https://schoolwijzer.amsterdam.nl/nl/';
+var options = {
+    filterLevel: 2,
+    excludeExternalLinks: true
+};
 
 //blc https://schoolwijzer.amsterdam.nl/nl/ -roe --filter-level=2
+
+
+var siteChecker = new blc.SiteChecker(options);
+
+//var siteChecker = new blc.SiteChecker(options, {
+    //robots: function(robots, customData){},
+    //html: function(tree, robots, response, pageUrl, customData){},
+    //junk: function(result, customData){},
+    //link: function(result, customData){},
+    //page: function(error, pageUrl, customData){},
+    //site: function(error, siteUrl, customData){},
+    //end: function(){}
+//});
+
+siteChecker.enqueue(siteUrl);
